7ec4b4b3933c2abf438f6715749fb522dc2ffb20
Roman <sankovich.roman@mail.ru>
2021-09-13 00:11:01 +0300

    first task loop complite

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 console.log(numberOfFilms);
 
@@ -9,12 +11,71 @@ const personalMovieDM = {
     privat: false
 };
 
-const lastFilm = prompt('Один из поледних простотренных фильмов?', '');
-const scoreLastFilm = prompt('На сколькл оцените его?', '');
+/* const a = prompt('Один из поледних простотренных фильмов?', '');
+const b = prompt('На сколькл оцените его?', '');
+const c = prompt('Один из поледних простотренных фильмов?', '');
+const d = prompt('На сколькл оцените его?', ''); */
 
-personalMovieDM.movies[lastFilm] = scoreLastFilm;
+/* personalMovieDM.movies[a] = b;
+personalMovieDM.movies[c] = d;
+ */
+                    /* 1 вариант */
+/* for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из поледних простотренных фильмов?', ''),
+          b = prompt('На сколькл оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDM.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }    
+} */
 
-console.log(lastFilm);
-console.log(scoreLastFilm);
+
+                    /* 2 вариант */
+/* let i = 0;
+
+do {
+    const a = prompt('Один из поледних простотренных фильмов?', ''),
+          b = prompt('На сколькл оцените его?', '');    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDM.movies[a] = b;
+        console.log('done');
+        i++;
+    } else {
+        console.log('error');
+    }
+}
+while (i < 2); */
+
+
+                    /* 3 вариант */
+
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из поледних простотренных фильмов?', ''),
+          b = prompt('На сколькл оцените его?', '');    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDM.movies[a] = b;
+        console.log('done');
+        i++;
+    } else {
+        console.log('error');
+    }
+}
+
+if (personalMovieDM.count < 10) {
+    console.log ('Просмотрено мало фильмов');
+} else if (personalMovieDM.count >= 10 && personalMovieDM.count < 30) {
+    console.log ('Вы классический зритель');
+} else if (personalMovieDM.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+/* console.log(a);
+console.log(b); */
 
 console.log(personalMovieDM);
